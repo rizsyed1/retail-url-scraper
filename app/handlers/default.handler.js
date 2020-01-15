9836dd9224446c65dbf22bf1cbe14d27c1690465
RizwanSyed357 <rizwansyed357@protonmail.com>
2020-01-15 14:23:51 +0000

    default handler added

--- app/handlers/default.handler.js
+++ app/handlers/default.handler.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const debug = require('..services/log.service')('handler:default');
+
+const path = '/';
+
+async function handler(req, res, next) {
+    log(handler.name);
+    res.json('Welcome to the Express app');
+};
+
+module.exports = {
+    handler, 
+    path
+};
